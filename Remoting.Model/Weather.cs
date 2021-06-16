69096dfff9a2723b8c849aea3926962cefdedace
adeyinka oluwaseun <seun.adeyinka@gmail.com>
2021-06-17 01:00:58 +0200

    Add project files.

---
+++ Remoting.Model/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Remoting.Model
+{
+    public class Weather : MarshalByRefObject
+    {
+        public string GetWeather(string city)
+        {
+            var client = new OpenWeatherAPI.API("ae27d47b28aeb52b51b119d944db0646");
+            var results = client.Query(city);
+            return $"The temperature in {city} is {results.Main.Temperature.CelsiusCurrent}C";
+        }
+    }
+}
---
+++ Remoting/Program.cs
@@ -0,0 +1,21 @@
+﻿using Remoting.Model;
+using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace Remoting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel tcpChannel = new TcpChannel(5050);
+            ChannelServices.RegisterChannel(tcpChannel);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Weather), 
+                "weather", WellKnownObjectMode.Singleton);
+            Console.Write("Server is ready to communicate..........");
+            Console.Read();
+        }
+    }
+}
